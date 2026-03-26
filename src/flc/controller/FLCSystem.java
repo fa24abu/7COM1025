f4d10adb55d2b8ea30fe7ae872233fcf1097f866
fa24abu <fa24abu@herts.ac.uk>
2026-03-27 00:27:34 +0500

    Create FLCSystem.java

---
+++ src/flc/controller/FLCSystem.java
@@ -0,0 +1,10 @@
+package flc.controller;
+
+public class FLCSystem {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Furzefield Leisure Centre Booking System");
+
+    }
+}
