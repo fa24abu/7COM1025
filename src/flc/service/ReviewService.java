76e7f5f0dd6aa4fcc5d41ee4a459f545d0547bb5
fa24abu <fa24abu@herts.ac.uk>
2026-03-31 23:52:53 +0500

    Update ReviewService.java

--- src/flc/service/ReviewService.java
+++ src/flc/service/ReviewService.java
@@ -1,4 +1,18 @@
 package flc.service;
 
+import flc.model.*;
+
 public class ReviewService {
+
+    public void addReview(Booking booking, String comment, int rating) {
+
+        if (booking == null) return;
+
+        if (!booking.getStatus().equals("attended")) {
+            System.out.println("You must attend lesson before reviewing!");
+            return;
+        }
+
+        booking.setReview(new Review(comment, rating));
+    }
 }
