71df8bbb4ac851aa5f94b83617f5449b370eff4d
fa24abu <fa24abu@herts.ac.uk>
2026-03-31 23:52:33 +0500

    Update TimeTableService.java

--- src/flc/service/TimeTableService.java
+++ src/flc/service/TimeTableService.java
@@ -1,4 +1,34 @@
 package flc.service;
 
+import flc.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimeTableService {
+    private List<Lesson> lessons = new ArrayList<>();
+
+    public void addLesson(Lesson lesson) {
+        lessons.add(lesson);
+    }
+
+    public List<Lesson> getAllLessons() { return lessons; }
+
+    public Lesson getLessonById(int id) {
+        return lessons.stream().filter(l -> l.getId() == id).findFirst().orElse(null);
+    }
+
+    public List<Lesson> getByDay(DayType day) {
+        List<Lesson> list = new ArrayList<>();
+        for (Lesson l : lessons)
+            if (l.getDay() == day) list.add(l);
+        return list;
+    }
+
+    public List<Lesson> getByType(ExerciseType type) {
+        List<Lesson> list = new ArrayList<>();
+        for (Lesson l : lessons)
+            if (l.getType() == type) list.add(l);
+        return list;
+    }
 }
