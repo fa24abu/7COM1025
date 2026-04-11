9dce71a8119a7c05ce28b100887a9c76e3975922
fa24abu <fa24abu@herts.ac.uk>
2026-04-11 14:44:07 +0500

    Change status type to BookingStatus enum

--- src/flc/model/Booking.java
+++ src/flc/model/Booking.java
@@ -4,14 +4,14 @@ public class Booking {
     private int id;
     private Member member;
     private Lesson lesson;
-    private String status;
+    private BookingStatus status;
     private Review review;
 
     public Booking(int id, Member member, Lesson lesson) {
         this.id = id;
         this.member = member;
         this.lesson = lesson;
-        this.status = "booked";
+        this.status = BookingStatus.BOOKED;
     }
 
     public int getId() { return id; }
@@ -20,8 +20,8 @@ public class Booking {
 
     public void setLesson(Lesson lesson) { this.lesson = lesson; }
 
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
+    public BookingStatus getStatus() { return status; }
+    public void setStatus(BookingStatus status) { this.status = status; }
 
     public void setReview(Review review) { this.review = review; }
     public Review getReview() { return review; }
